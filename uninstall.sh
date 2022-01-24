9c5da708c06fb78589b65b658338a9e97026719f
Fox <xuandung38@gmail.com>
2022-01-24 16:49:09 +0700

    Add Uninstall command

---
+++ uninstall.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# UnInstall MEMP on Applesilicon.
+# Script by xuandung38
+
+# Helpers:
+txtbold=$(tput bold)
+boldyellow=${txtbold}$(tput setaf 3)
+boldgreen=${txtbold}$(tput setaf 2)
+yellow=$(tput setaf 3)
+green=$(tput setaf 2)
+txtreset=$(tput sgr0)
+
+echo "${boldgreen}Start${txtreset}"
+
+brew services stop nginx
+sudo brew services stop dnsmasq
+brew services stop redis
+brew services stop mysql
+brew uninstall nginx
+brew uninstall --ignore-dependencies php
+brew uninstall dnsmasq
+brew uninstall redis
+brew uninstall mysql
+sudo rm /opt/homebrew/etc/dnsmasq.conf
+sudo rm /etc/resolver/web
+
+echo "${boldgreen}Remove MEMP Success${txtreset}"
